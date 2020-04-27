a44af673aea8cefd69d0d201f5cbcae4f53d8116
Thomas Kuenneth <kuennetht@gmail.com>
2020-04-27 11:43:06 +0200

    Project InstanceStateDemo added

---
+++ kapitel_04/InstanceStateDemo/app/src/main/java/com/thomaskuenneth/androidbuch/instancestatedemo/InstanceStateDemoActivity.kt
@@ -0,0 +1,32 @@
+package com.thomaskuenneth.androidbuch.instancestatedemo
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+private val TAG = InstanceStateDemoActivity::class.simpleName
+
+class InstanceStateDemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            Log.d(TAG, "savedInstanceState war null")
+        } else {
+            val time = System.currentTimeMillis() - savedInstanceState
+                    .getLong(TAG)
+            Log.d(TAG, "wurde vor $time Millisekunden beendet")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong(TAG, System.currentTimeMillis())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState()")
+    }
+}
