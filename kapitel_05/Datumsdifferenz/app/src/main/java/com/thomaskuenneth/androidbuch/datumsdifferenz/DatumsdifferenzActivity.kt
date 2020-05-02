3df30875dc31fae0b4053901076d2daf6b0d37bc
Thomas Kuenneth <kuennetht@gmail.com>
2020-05-02 15:11:02 +0200

    Back to java.util.Calendar

--- kapitel_05/Datumsdifferenz/app/src/main/java/com/thomaskuenneth/androidbuch/datumsdifferenz/DatumsdifferenzActivity.kt
+++ kapitel_05/Datumsdifferenz/app/src/main/java/com/thomaskuenneth/androidbuch/datumsdifferenz/DatumsdifferenzActivity.kt
@@ -1,6 +1,6 @@
 package com.thomaskuenneth.androidbuch.datumsdifferenz
 
-import android.icu.util.Calendar
+import java.util.Calendar
 import android.os.Bundle
 import android.widget.Button
 import android.widget.DatePicker
@@ -19,21 +19,16 @@ class DatumsdifferenzActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.datumsdifferenz)
-        // val callback = DatePicker.OnDateChangedListener() { _, _, _, _ -> berechnen() }
         dp1 = findViewById(R.id.date1)
-        dp1.init(
-            cal1.get(Calendar.YEAR),
+        dp1.init(cal1.get(Calendar.YEAR),
             cal1.get(Calendar.MONTH),
             cal1.get(Calendar.DAY_OF_MONTH),
-            null // callback
-        )
+            null)
         dp2 = findViewById(R.id.date2)
-        dp2.init(
-            cal2.get(Calendar.YEAR),
+        dp2.init(cal2.get(Calendar.YEAR),
             cal2.get(Calendar.MONTH),
             cal2.get(Calendar.DAY_OF_MONTH),
-            null // callback
-        )
+            null)
         tv = findViewById(R.id.textview_result)
         val b = findViewById<Button>(R.id.button_calc)
         b.setOnClickListener { berechnen() }
@@ -51,18 +46,15 @@ class DatumsdifferenzActivity : AppCompatActivity() {
         var days = 0
         while (cal1[Calendar.YEAR] != cal2[Calendar.YEAR]
             || cal1[Calendar.MONTH] != cal2[Calendar.MONTH]
-            || cal1[Calendar.DAY_OF_MONTH] != cal2[Calendar.DAY_OF_MONTH]
-        ) {
+            || cal1[Calendar.DAY_OF_MONTH] != cal2[Calendar.DAY_OF_MONTH]) {
             days += 1
             cal1.add(Calendar.DAY_OF_YEAR, 1)
         }
         tv.text = getString(R.string.template, days)
     }
 
-    private fun updateCalendarFromDatePicker(
-        cal: Calendar,
-        dp: DatePicker
-    ) {
+    private fun updateCalendarFromDatePicker(cal: Calendar,
+                                             dp: DatePicker) {
         cal[Calendar.YEAR] = dp.year
         cal[Calendar.MONTH] = dp.month
         cal[Calendar.DAY_OF_MONTH] = dp.dayOfMonth
