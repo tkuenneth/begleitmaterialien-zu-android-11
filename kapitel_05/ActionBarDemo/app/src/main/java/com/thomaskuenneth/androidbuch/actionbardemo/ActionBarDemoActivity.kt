d98953cc1ef1e0aef2cfae190102adcc69dd60d7
Thomas Kuenneth <kuennetht@gmail.com>
2020-05-05 13:13:39 +0200

    Project ActionBarDemo added

---
+++ kapitel_05/ActionBarDemo/app/src/main/java/com/thomaskuenneth/androidbuch/actionbardemo/ActionBarDemoActivity.kt
@@ -0,0 +1,42 @@
+package com.thomaskuenneth.androidbuch.actionbardemo
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ActionBarDemoActivity : AppCompatActivity() {
+
+    private lateinit var textview: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textview = findViewById(R.id.textview)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (android.R.id.home == item.itemId) {
+            Toast.makeText(
+                this, R.string.app_name,
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            textview.text = item.title
+        }
+        return true
+    }
+}
