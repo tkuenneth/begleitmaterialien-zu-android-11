19f15221f048478795c825ba9908741bbf21cfdd
Thomas Kuenneth <kuennetht@gmail.com>
2020-05-09 11:52:26 +0200

    Project SnackbarDemo added

---
+++ kapitel_05/SnackbarDemo/app/src/main/java/com/thomaskuenneth/androidbuch/snackbardemo/SnackbarDemoActivity.kt
@@ -0,0 +1,26 @@
+package com.thomaskuenneth.androidbuch.snackbardemo
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+class SnackbarDemoActivity : AppCompatActivity() {
+
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val info = findViewById<TextView>(R.id.info)
+        button.setOnClickListener {
+            val snackbar = Snackbar.make(button, R.string.info, Snackbar.LENGTH_LONG)
+            snackbar.setAction(R.string.action) {
+                info.text = getString(R.string.template, ++count)
+            }
+            snackbar.show()
+        }
+    }
+}
