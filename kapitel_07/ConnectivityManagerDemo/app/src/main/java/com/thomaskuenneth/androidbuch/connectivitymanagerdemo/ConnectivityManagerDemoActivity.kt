a2fa84e01e600f99410f8a2f567ac095b75462af
Thomas Kuenneth <thomas@thomaskuenneth.com>
2020-05-29 15:34:45 +0200

    Project ConnectivityManagerDemo added

---
+++ kapitel_07/ConnectivityManagerDemo/app/src/main/java/com/thomaskuenneth/androidbuch/connectivitymanagerdemo/ConnectivityManagerDemoActivity.kt
@@ -0,0 +1,31 @@
+package com.thomaskuenneth.androidbuch.connectivitymanagerdemo
+
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ConnectivityManagerDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mgr = getSystemService(ConnectivityManager::class.java)
+        mgr?.allNetworks?.forEach {
+            val properties = mgr.getLinkProperties(it)
+            textview.append("${properties?.interfaceName}\n")
+            val capabilities = mgr.getNetworkCapabilities(it)
+            val notRoaming = capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_ROAMING)
+                    ?: true
+            textview.append("Roaming ist ${if (notRoaming) "aus" else "ein"}\n")
+            // ab API-Level 29 vorhanden
+            // textview.append("Signalstärke: ${capabilities?.signalStrength}\n")
+            val foreground = capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_FOREGROUND)
+                    ?: false
+            textview.append("Nutzbar durch Apps: $foreground\n")
+            if (textview.text.isNotEmpty()) {
+                textview.append("\n")
+            }
+        }
+    }
+}
