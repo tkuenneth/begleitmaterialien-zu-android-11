2005c2763cb4709f964f8c2975e3ac0e784d3d51
Thomas Kuenneth <thomas@thomaskuenneth.com>
2020-05-31 11:54:53 +0200

    Update WebViewDemo3Activity.kt

--- kapitel_07/WebViewDemo3/app/src/main/java/com/thomaskuenneth/androidbuch/webviewdemo3/WebViewDemo3Activity.kt
+++ kapitel_07/WebViewDemo3/app/src/main/java/com/thomaskuenneth/androidbuch/webviewdemo3/WebViewDemo3Activity.kt
@@ -2,6 +2,9 @@ package com.thomaskuenneth.androidbuch.webviewdemo3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.webkit.WebResourceRequest
+//import android.webkit.WebView
+//import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
 class WebViewDemo3Activity : AppCompatActivity() {
@@ -9,6 +12,17 @@ class WebViewDemo3Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         webview.settings.javaScriptEnabled = true
+//        webview.addJavascriptInterface(WebAppInterface(this), "Android")
+//        val client = object : WebViewClient() {
+//            override fun shouldOverrideUrlLoading(
+//                view: WebView,
+//                request: WebResourceRequest
+//            ): Boolean {
+//                return false
+//            }
+//        }
+//        webview.webViewClient = client
         webview.loadUrl("file:/android_asset/test1.html")
+//        webview.loadUrl("file:///android_asset/test2.html")
     }
 }
