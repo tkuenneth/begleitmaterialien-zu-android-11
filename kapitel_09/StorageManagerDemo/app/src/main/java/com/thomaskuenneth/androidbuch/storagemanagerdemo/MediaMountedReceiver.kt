0a65c88fa7c8f46eae79d2cf942448282093dc48
Thomas Kuenneth <thomas@thomaskuenneth.com>
2020-06-13 13:43:39 +0200

    Update MediaMountedReceiver.kt

--- kapitel_09/StorageManagerDemo/app/src/main/java/com/thomaskuenneth/androidbuch/storagemanagerdemo/MediaMountedReceiver.kt
+++ kapitel_09/StorageManagerDemo/app/src/main/java/com/thomaskuenneth/androidbuch/storagemanagerdemo/MediaMountedReceiver.kt
@@ -1,2 +1,23 @@
 package com.thomaskuenneth.androidbuch.storagemanagerdemo
 
+import android.content.*
+import android.os.storage.StorageVolume
+import android.widget.Toast
+
+class MediaMountedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent != null &&
+            Intent.ACTION_MEDIA_MOUNTED == intent.action
+        ) {
+            val volume = intent.getParcelableExtra<StorageVolume>(
+                StorageVolume.EXTRA_STORAGE_VOLUME
+            )
+            if (volume != null) {
+                Toast.makeText(
+                    context, volume.getDescription(context),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+}
