d6156d3c8fba273491d303e6606d813daeeb2923
Thomas Kuenneth <thomas@thomaskuenneth.com>
2020-06-17 14:22:17 +0200

    Project DBDemo1 added

---
+++ kapitel_10/DBDemo1/app/src/main/java/com/thomaskuenneth/androidbuch/dbdemo1/DBDemo1Activity.kt
@@ -0,0 +1,30 @@
+package com.thomaskuenneth.androidbuch.dbdemo1
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class DBDemo1Activity : AppCompatActivity() {
+    private lateinit var openHelper: DBDemo1OpenHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fine.setOnClickListener { imageButtonClicked(MOOD_FINE) }
+        ok.setOnClickListener { imageButtonClicked(MOOD_OK) }
+        bad.setOnClickListener { imageButtonClicked(MOOD_BAD) }
+        openHelper = DBDemo1OpenHelper(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        openHelper.close()
+    }
+
+    private fun imageButtonClicked(mood: Int) {
+        openHelper.insert(mood, System.currentTimeMillis())
+        Toast.makeText(this, R.string.saved, Toast.LENGTH_SHORT)
+            .show()
+    }
+}
---
+++ kapitel_10/DBDemo1/app/src/main/java/com/thomaskuenneth/androidbuch/dbdemo1/DBDemo1OpenHelper.kt
@@ -0,0 +1,75 @@
+package com.thomaskuenneth.androidbuch.dbdemo1
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+// Konstanten für die Stimmungen
+const val MOOD_FINE = 1
+const val MOOD_OK = 2
+const val MOOD_BAD = 3
+
+// Name und Version der Datenbank
+private const val DATABASE_NAME = "tkmoodley.db"
+private const val DATABASE_VERSION = 1
+
+private val TAG = DBDemo1OpenHelper::class.simpleName
+class DBDemo1OpenHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    // Name und Attribute der Tabelle "mood"
+    private val id = "_id"
+    private val tableMoodName = "mood"
+    private val moodTime = "timeMillis"
+    private val moodMood = "mood"
+
+    // Tabelle "mood" anlegen
+    private val tableMoodCreate = """
+        CREATE TABLE $tableMoodName (
+        $id INTEGER PRIMARY KEY AUTOINCREMENT,
+        $moodTime INTEGER,
+        $moodMood INTEGER);")        
+    """.trimIndent()
+
+    // Tabelle "mood" löschen
+    private val tableMoodDrop = "DROP TABLE IF EXISTS $tableMoodName"
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(tableMoodCreate)
+    }
+
+    override fun onUpgrade(
+        db: SQLiteDatabase, oldVersion: Int,
+        newVersion: Int
+    ) {
+        Log.w(
+            TAG, """
+                Upgrade der Datenbank von Version $oldVersion zu $newVersion.
+                Alle Daten werden gelöscht."                
+            """.trimIndent()
+        )
+        db.execSQL(tableMoodDrop)
+        onCreate(db)
+    }
+
+    fun insert(mood: Int, timeMillis: Long) {
+        var rowId = -1L
+        try {
+            // Datenbank öffnen
+            Log.d(TAG, "Pfad: " + writableDatabase.path)
+            // die zu speichernden Werte
+            val values = ContentValues()
+            values.put(moodMood, mood)
+            values.put(moodTime, timeMillis)
+            // in die Tabelle "mood" einfügen
+            rowId = writableDatabase.insert(tableMoodName, null, values)
+        } catch (e: SQLiteException) {
+            Log.e(TAG, "insert()", e)
+        } finally {
+            Log.d(TAG, "insert(): rowId=$rowId")
+        }
+    }
+}
