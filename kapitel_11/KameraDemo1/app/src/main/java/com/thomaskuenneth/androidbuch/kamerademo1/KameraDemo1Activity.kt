f86d6e306d87679337f7e32c1b9171ad5a7845c1
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-06-27 12:53:49 +0200

    Project KameraDemo1 added

---
+++ kapitel_11/KameraDemo1/app/src/main/java/com/thomaskuenneth/androidbuch/kamerademo1/KameraDemo1Activity.kt
@@ -0,0 +1,24 @@
+package com.thomaskuenneth.androidbuch.kamerademo1
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class KameraDemo1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        foto.setOnClickListener { v ->
+            val intent = Intent(
+                    MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA)
+            startActivity(intent)
+        }
+        video.setOnClickListener {
+            val intent = Intent(
+                    MediaStore.INTENT_ACTION_VIDEO_CAMERA)
+            startActivity(intent)
+        }
+    }
+}
