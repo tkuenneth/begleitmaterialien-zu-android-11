da5dc4477778b8f95cf4aed13d45ba45c3e1d6ca
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-07-04 13:01:11 +0200

    work in progress

--- kapitel_11/KameraDemo3/app/src/main/java/com/thomaskuenneth/androidbuch/kamerademo3/KameraDemo3Activity.kt
+++ kapitel_11/KameraDemo3/app/src/main/java/com/thomaskuenneth/androidbuch/kamerademo3/KameraDemo3Activity.kt
@@ -1,35 +1,45 @@
 package com.thomaskuenneth.androidbuch.kamerademo3
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.ImageFormat
 import android.hardware.camera2.*
-import android.hardware.camera2.CameraCaptureSession.CaptureCallback
 import android.hardware.camera2.params.OutputConfiguration
 import android.hardware.camera2.params.SessionConfiguration
+import android.media.ImageReader
+import android.media.MediaScannerConnection
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.util.Size
 import android.view.SurfaceHolder
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
 
 private val TAG = KameraDemo3Activity::class.simpleName
-
 class KameraDemo3Activity : AppCompatActivity() {
     private val requestCamera = 123
     private var camera: CameraDevice? = null
     private var activeSession: CameraCaptureSession? = null
-    private var builderPreview: CaptureRequest.Builder? = null
-    private val captureCallback: CaptureCallback? = null
+
+    private lateinit var manager: CameraManager
+    private lateinit var cameraId: String
+    private lateinit var imageReader: ImageReader
+    private lateinit var builderPreview: CaptureRequest.Builder
+
     private val captureSessionCallback = object : CameraCaptureSession.StateCallback() {
         override fun onConfigured(session: CameraCaptureSession) {
             try {
-                builderPreview?.build()?.let {
-                    session.setRepeatingRequest(it, captureCallback, null)
-                    activeSession = session
-                }
+                session.setRepeatingRequest(builderPreview.build(), null, null)
+                activeSession = session
             } catch (e: CameraAccessException) {
                 Log.e(TAG, "onConfigured()", e)
             }
@@ -39,6 +49,7 @@ class KameraDemo3Activity : AppCompatActivity() {
             Log.e(TAG, "onConfigureFailed()")
         }
     }
+
     private val surfaceHolderCallback: SurfaceHolder.Callback = object : SurfaceHolder.Callback {
         override fun surfaceDestroyed(holder: SurfaceHolder) {
             Log.d(TAG, "surfaceDestroyed()")
@@ -48,22 +59,20 @@ class KameraDemo3Activity : AppCompatActivity() {
             Log.d(TAG, "surfaceCreated()")
             try {
                 openCamera()
-            } catch (e: SecurityException) {
-                Log.e(TAG, "openCamera()", e)
-            } catch (e: CameraAccessException) {
+            } catch (e: Exception) {
+                // SecurityException, CameraAccessException
                 Log.e(TAG, "openCamera()", e)
             }
         }
 
-        override fun surfaceChanged(holder: SurfaceHolder,
-                                    format: Int, width: Int,
-                                    height: Int) {
+        override fun surfaceChanged(
+            holder: SurfaceHolder,
+            format: Int, width: Int,
+            height: Int
+        ) {
             Log.d(TAG, "surfaceChanged()")
         }
     }
-
-    private lateinit var manager: CameraManager
-    private lateinit var cameraId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,38 +84,75 @@ class KameraDemo3Activity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         surfaceview.visibility = View.GONE
-        activeSession?.let {
-            it.close()
-            activeSession = null
-        }
-        camera?.let {
-            it.close()
-            camera = null
-        }
+        activeSession?.close()
+        activeSession = null
+        camera?.close()
+        camera = null
         surfaceview.holder.removeCallback(surfaceHolderCallback)
-        Log.d(TAG, "onPause()")
     }
 
     override fun onResume() {
         super.onResume()
         if (checkSelfPermission(Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(arrayOf(Manifest.permission.CAMERA),
-                    requestCamera)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.CAMERA),
+                requestCamera
+            )
         } else {
             configureHolder()
         }
-        Log.d(TAG, "onResume()")
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>,
-                                            grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         if (requestCode == requestCamera &&
-                grantResults.isNotEmpty() && grantResults[0] ==
-                PackageManager.PERMISSION_GRANTED) {
+            grantResults.isNotEmpty() && grantResults[0] ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
             configureHolder()
         }
+    }
+
+    private fun configureHolder() {
+        surfaceview.holder.addCallback(surfaceHolderCallback)
+        val sizes = findCameraFacingBack()
+        if (sizes.isEmpty()) {
+            Log.d(TAG, "keine passende Kamera gefunden")
+            finish()
+        }
+        val metrics = resources.displayMetrics
+        for (size in sizes) {
+            if (size.width > metrics.widthPixels
+                || size.height > metrics.heightPixels
+            ) {
+                continue
+            }
+            surfaceview.setOnClickListener { takePicture() }
+            surfaceview.holder.setFixedSize(size.width, size.height)
+            surfaceview.visibility = View.VISIBLE
+            imageReader = ImageReader.newInstance(
+                size.width, size.height,
+                ImageFormat.JPEG, 2
+            )
+            imageReader.setOnImageAvailableListener(
+                {
+                    Log.d(TAG, "setOnImageAvailableListener()")
+                    val image = imageReader.acquireLatestImage()
+                    val planes = image.planes
+                    val buffer = planes[0].buffer
+                    saveJPG(buffer)
+                    image.close()
+                }, null
+            )
+            return
+        }
+        Log.d(TAG, "Zu groß")
+        finish()
     }
 
     // vorhandene Kameras ermitteln und auswählen
@@ -119,11 +165,13 @@ class KameraDemo3Activity : AppCompatActivity() {
                 Log.d(TAG, "$id: $cc")
                 val lensFacing = cc.get(CameraCharacteristics.LENS_FACING)
                 if (lensFacing != null &&
-                        lensFacing ==
-                        CameraCharacteristics.LENS_FACING_BACK) {
+                    lensFacing ==
+                    CameraCharacteristics.LENS_FACING_BACK
+                ) {
                     cameraId = id
                     val configs = cc.get(
-                            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                        CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP
+                    )
                     if (configs != null) {
                         return configs.getOutputSizes(SurfaceHolder::class.java)
                     }
@@ -137,61 +185,103 @@ class KameraDemo3Activity : AppCompatActivity() {
         return emptyArray()
     }
 
-    private fun configureHolder() {
-        surfaceview.holder.addCallback(surfaceHolderCallback)
-        val sizes = findCameraFacingBack()
-        if (sizes.isEmpty()) {
-            Log.d(TAG, "keine passende Kamera gefunden")
-            finish()
-        }
-        val metrics = resources.displayMetrics
-        for (size in sizes) {
-            if (size.width > metrics.widthPixels
-                    || size.height > metrics.heightPixels) {
-                continue
-            }
-            surfaceview.holder.setFixedSize(size.width, size.height)
-            surfaceview.visibility = View.VISIBLE
-            return
-        }
-        Log.d(TAG, "Zu groß")
-        finish()
-    }
-
     @Throws(SecurityException::class)
     private fun openCamera() {
-        manager.openCamera(cameraId,
-                object : CameraDevice.StateCallback() {
-                    override fun onOpened(_camera: CameraDevice) {
-                        Log.d(TAG, "onOpened()")
-                        camera = _camera
-                        createPreviewCaptureSession()
-                    }
+        manager.openCamera(
+            cameraId,
+            object : CameraDevice.StateCallback() {
+                override fun onOpened(_camera: CameraDevice) {
+                    Log.d(TAG, "onOpened()")
+                    camera = _camera
+                    createPreviewCaptureSession()
+                }
 
-                    override fun onDisconnected(camera: CameraDevice) {
-                        Log.d(TAG, "onDisconnected()")
-                    }
+                override fun onDisconnected(camera: CameraDevice) {
+                    Log.d(TAG, "onDisconnected()")
+                }
 
-                    override fun onError(camera: CameraDevice,
-                                         error: Int) {
-                        Log.d(TAG, "onError()")
-                    }
-                }, null)
+                override fun onError(
+                    camera: CameraDevice,
+                    error: Int
+                ) {
+                    Log.d(TAG, "onError()")
+                }
+            }, null
+        )
     }
 
     private fun createPreviewCaptureSession() {
-        surfaceview.holder.surface?.let {
-            val outputs = arrayListOf(OutputConfiguration(it))
-            val sessionConfiguration = SessionConfiguration(SessionConfiguration.SESSION_REGULAR,
-                    outputs, mainExecutor, captureSessionCallback)
-            try {
-                builderPreview = camera?.createCaptureRequest(
-                        CameraDevice.TEMPLATE_PREVIEW)
-                builderPreview?.addTarget(it)
+        val outputs = mutableListOf<OutputConfiguration>()
+        outputs.add(OutputConfiguration(surfaceview.holder.surface))
+        outputs.add(OutputConfiguration(imageReader.surface))
+        val sessionConfiguration = SessionConfiguration(
+            SessionConfiguration.SESSION_REGULAR,
+            outputs, mainExecutor, captureSessionCallback
+        )
+        try {
+            camera?.createCaptureRequest(
+                CameraDevice.TEMPLATE_PREVIEW
+            )?.let {
+                builderPreview = it
+                it.addTarget(surfaceview.holder.surface)
                 camera?.createCaptureSession(sessionConfiguration)
-            } catch (e: Exception) {
-                Log.e(TAG, "createPreviewCaptureSession()", e)
             }
+        } catch (e: Exception) {
+            Log.e(TAG, "createPreviewCaptureSession()", e)
+        }
+    }
+
+    private fun takePicture() {
+        try {
+            // für die Aufnahme
+            val builder = camera?.createCaptureRequest(
+                CameraDevice.TEMPLATE_STILL_CAPTURE
+            )
+            builder?.addTarget(imageReader.surface)
+            builder?.build()?.let {
+                activeSession?.capture(it, null, null)
+            }
+        } catch (e: CameraAccessException) {
+            Log.e(TAG, "takePicture()", e)
+        }
+    }
+
+    private fun saveJPG(data: ByteBuffer) {
+        getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.let {
+            if (it.mkdirs()) {
+                Log.d(TAG, "dirs created")
+            }
+            val f = File(it, "${TAG}_${System.currentTimeMillis()}.jpg")
+            try {
+                FileOutputStream(f).use { fos ->
+                    BufferedOutputStream(fos).use { bos ->
+                        while (data.hasRemaining()) {
+                            bos.write(data.get().toInt())
+                        }
+                        Toast.makeText(
+                            this, R.string.click,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        addToMediaProvider(f)
+                    }
+                }
+            } catch (e: IOException) {
+                Log.e(TAG, "saveJPG()", e)
+            }
+        }
+    }
+
+    private fun addToMediaProvider(f: File) {
+        MediaScannerConnection.scanFile(
+            this,
+            arrayOf(f.toString()),
+            arrayOf("image/jpeg")
+        ) { _, uri ->
+            val i = Intent(
+                Intent.ACTION_VIEW,
+                uri
+            )
+            startActivity(i)
         }
     }
 }
