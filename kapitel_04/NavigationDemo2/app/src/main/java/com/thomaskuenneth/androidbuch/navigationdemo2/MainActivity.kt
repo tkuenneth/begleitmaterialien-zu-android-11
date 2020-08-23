5566f41f3477c396f3d2fbec95d299dd7899b205
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-08-23 14:32:12 +0200

    Project NavigationDemo2 added

---
+++ kapitel_04/NavigationDemo2/app/src/main/java/com/thomaskuenneth/androidbuch/navigationdemo2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.thomaskuenneth.androidbuch.navigationdemo2
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textview = findViewById<TextView>(R.id.textview)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_home -> {
+                    textview.text = getString(R.string.home)
+                    true
+                }
+                R.id.page_info -> {
+                    textview.text = getString(R.string.info)
+                    true
+                }
+                else -> false
+            }
+        }
+        bottomNavigationView.selectedItemId = R.id.page_home
+    }
+}
