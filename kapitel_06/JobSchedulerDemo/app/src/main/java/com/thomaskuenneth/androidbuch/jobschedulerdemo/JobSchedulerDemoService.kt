928c87e8eab4fb703be77b23fc25750709acfac5
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-01 16:01:27 +0200

    Update JobSchedulerDemoService.kt

--- kapitel_06/JobSchedulerDemo/app/src/main/java/com/thomaskuenneth/androidbuch/jobschedulerdemo/JobSchedulerDemoService.kt
+++ kapitel_06/JobSchedulerDemo/app/src/main/java/com/thomaskuenneth/androidbuch/jobschedulerdemo/JobSchedulerDemoService.kt
@@ -1,21 +1,20 @@
 package com.thomaskuenneth.androidbuch.jobschedulerdemo
 
-import android.app.job.JobParameters
-import android.app.job.JobService
+import android.app.job.*
 import android.util.Log
 import android.widget.Toast
+import kotlin.concurrent.thread
 
 private val TAG = JobSchedulerDemoService::class.simpleName
-
 class JobSchedulerDemoService : JobService() {
     override fun onStartJob(params: JobParameters?): Boolean {
         Log.d(TAG, "onStartJob()")
         Toast.makeText(this, R.string.job_started, Toast.LENGTH_LONG)
             .show()
-        Thread {
+        thread {
             Log.d(TAG, "Job in Aktion")
             jobFinished(params, false)
-        }.start()
+        }
         return true
     }
 
