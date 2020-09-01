c012a89480f769244fab373f1940678a01d1bb1a
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-01 16:10:29 +0200

    Minor refactoring

--- kapitel_06/WorkManagerDemo/app/src/main/java/com/thomaskuenneth/androidbuch/workmanagerdemo/WorkManagerDemoActivity.kt
+++ kapitel_06/WorkManagerDemo/app/src/main/java/com/thomaskuenneth/androidbuch/workmanagerdemo/WorkManagerDemoActivity.kt
@@ -1,26 +1,25 @@
 package com.thomaskuenneth.androidbuch.workmanagerdemo
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.*
+import kotlinx.android.synthetic.main.activity_main.*
 import java.time.Duration
 
 class WorkManagerDemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun handleButtonClicked(view: View) {
-        val constraints = Constraints.Builder()
-            .setRequiresCharging(true)
-            .build()
-        val repeatInterval = Duration.ofMillis(PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS)
-        val work = PeriodicWorkRequestBuilder<DemoWorker>(repeatInterval)
-            .setConstraints(constraints)
-            .setInputData(Data.Builder().putInt(KeyNumber, 123).build())
-            .build()
-        WorkManager.getInstance(this).enqueue(work)
+        start.setOnClickListener {
+            val constraints = Constraints.Builder()
+                .setRequiresCharging(true)
+                .build()
+            val repeatInterval = Duration.ofMillis(PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS)
+            val work = PeriodicWorkRequestBuilder<DemoWorker>(repeatInterval)
+                .setConstraints(constraints)
+                .setInputData(Data.Builder().putInt(KeyNumber, 123).build())
+                .build()
+            WorkManager.getInstance(this).enqueue(work)
+        }
     }
 }
