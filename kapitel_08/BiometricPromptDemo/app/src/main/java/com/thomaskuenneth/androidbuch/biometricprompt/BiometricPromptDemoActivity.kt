a10d2a235eb5fd17f5bb03ca16a5aa3fd083f2e8
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-03 14:50:26 +0200

    Migrated to Jetpack

--- kapitel_08/BiometricPromptDemo/app/src/main/java/com/thomaskuenneth/androidbuch/biometricprompt/BiometricPromptDemoActivity.kt
+++ kapitel_08/BiometricPromptDemo/app/src/main/java/com/thomaskuenneth/androidbuch/biometricprompt/BiometricPromptDemoActivity.kt
@@ -1,25 +1,30 @@
 package com.thomaskuenneth.androidbuch.biometricprompt
 
-import android.content.DialogInterface
-import android.hardware.biometrics.BiometricPrompt
 import android.os.Bundle
-import android.os.CancellationSignal
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricPrompt
 import kotlinx.android.synthetic.main.activity_main.*
 
 class BiometricPromptDemoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val prompt = BiometricPrompt.Builder(this)
+        button.setOnClickListener { showDialog() }
+    }
+
+    private fun toast(resid: Int) = Toast.makeText(this, resid, Toast.LENGTH_LONG).show()
+
+    private fun showDialog() {
+        val info = BiometricPrompt.PromptInfo.Builder()
                 .setDescription(getString(R.string.descr))
                 .setTitle(getString(R.string.title))
+                .setConfirmationRequired(true)
                 .setSubtitle(getString(R.string.subtitle))
-                .setNegativeButton(getString(R.string.button),
-                        mainExecutor, { _: DialogInterface?, _: Int -> toast(R.string.clicked) }
-                ).build()
-        prompt.authenticate(CancellationSignal(), mainExecutor,
+                .setNegativeButtonText(getString(R.string.cancel))
+                .build()
+        BiometricPrompt(this, mainExecutor,
                 object : BiometricPrompt.AuthenticationCallback() {
                     override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
                         toast(R.string.error)
@@ -32,11 +37,6 @@ class BiometricPromptDemoActivity : AppCompatActivity() {
                     override fun onAuthenticationFailed() {
                         toast(R.string.failed)
                     }
-                })
-    }
-
-    private fun toast(resid: Int) {
-        Toast.makeText(this, resid, Toast.LENGTH_LONG).show()
-        textview.setText(resid)
+                }).authenticate(info)
     }
 }
