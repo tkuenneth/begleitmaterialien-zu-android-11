d377806383d3efda0f700ce198644861c68e73b1
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-03 13:09:01 +0200

    Update SocketThread.kt

--- kapitel_08/BluetoothChatDemo/app/src/main/java/com/thomaskuenneth/androidbuch/bluetoothchatdemo/SocketThread.kt
+++ kapitel_08/BluetoothChatDemo/app/src/main/java/com/thomaskuenneth/androidbuch/bluetoothchatdemo/SocketThread.kt
@@ -3,8 +3,6 @@ package com.thomaskuenneth.androidbuch.bluetoothchatdemo
 import android.bluetooth.BluetoothSocket
 
 abstract class SocketThread : Thread() {
-
     abstract fun getSocket(): BluetoothSocket?
-
     abstract fun cancel()
 }
