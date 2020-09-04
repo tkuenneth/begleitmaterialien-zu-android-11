24927c6fc4be5a62bece4ced161e5bb6184c874d
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-04 13:49:29 +0200

    Update FileDemo3Activity.kt

--- kapitel_09/FileDemo3/app/src/main/java/com/thomaskuenneth/androidbuch/filedemo3/FileDemo3Activity.kt
+++ kapitel_09/FileDemo3/app/src/main/java/com/thomaskuenneth/androidbuch/filedemo3/FileDemo3Activity.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
+import java.io.*
 
 private val TAG = FileDemo3Activity::class.simpleName
 class FileDemo3Activity : AppCompatActivity() {
@@ -28,7 +26,8 @@ class FileDemo3Activity : AppCompatActivity() {
         createFile(dirVideo, "F")
         // temporäre Datei anlegen
         try {
-            Log.d(TAG, "java.io.tmpdir: ${System.getProperty("java.io.tmpdir")}")
+            Log.d(TAG, "java.io.tmpdir: ${
+                System.getProperty("java.io.tmpdir")}")
             val file = File.createTempFile("Datei_", ".txt")
             file.let {
                 Log.d(TAG, "---> ${it.absolutePath}")
@@ -40,7 +39,7 @@ class FileDemo3Activity : AppCompatActivity() {
         Log.d(TAG, "cacheDir: ${cacheDir.absolutePath}")
         try {
             val file2 = File.createTempFile("Datei_", ".txt",
-                    cacheDir)
+                cacheDir)
             Log.d(TAG, "---> ${file2.absolutePath}")
         } catch (e: IOException) {
             Log.e(TAG, " createTempFile()", e)
