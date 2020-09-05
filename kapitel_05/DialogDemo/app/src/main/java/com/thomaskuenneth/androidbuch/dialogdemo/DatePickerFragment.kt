df0481e687c3268d2c0ce2c1a07a9cb60404c990
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-05 09:05:08 +0200

    Update DatePickerFragment.kt

--- kapitel_05/DialogDemo/app/src/main/java/com/thomaskuenneth/androidbuch/dialogdemo/DatePickerFragment.kt
+++ kapitel_05/DialogDemo/app/src/main/java/com/thomaskuenneth/androidbuch/dialogdemo/DatePickerFragment.kt
@@ -21,10 +21,10 @@ class DatePickerFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         // das aktuelle Datum als Voreinstellung nehmen
-        val c: Calendar = Calendar.getInstance()
-        val year: Int = c.get(Calendar.YEAR)
-        val month: Int = c.get(Calendar.MONTH)
-        val day: Int = c.get(Calendar.DAY_OF_MONTH)
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
         // einen DatePickerDialog erzeugen und zurückliefern
         return DatePickerDialog(
             requireContext(),
