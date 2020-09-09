0fa139b9997854ed5db68d76454709a5bf8041d7
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-09 15:18:21 +0200

    Update DBDemo2Adapter.kt

--- kapitel_10/DBDemo2/app/src/main/java/com/thomaskuenneth/androidbuch/dbdemo2/DBDemo2Adapter.kt
+++ kapitel_10/DBDemo2/app/src/main/java/com/thomaskuenneth/androidbuch/dbdemo2/DBDemo2Adapter.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import android.database.Cursor
 import android.view.*
 import android.widget.*
-import java.text.DateFormat
-import java.text.SimpleDateFormat
+import java.text.*
 import java.util.*
 
-class DBDemo2Adapter(context: Context?) : CursorAdapter(context, null, 0) {
+class DBDemo2Adapter(context: Context?) : CursorAdapter(context, null,
+    0) {
     private val dateFormat = SimpleDateFormat
         .getDateInstance(DateFormat.MEDIUM)
     private val timeFormat = SimpleDateFormat
@@ -24,32 +24,23 @@ class DBDemo2Adapter(context: Context?) : CursorAdapter(context, null, 0) {
 
     override fun bindView(view: View?, context: Context?,
                           cursor: Cursor?) {
-        val ciMood = cursor?.getColumnIndex(MOOD_MOOD)
-        ciMood?.let {
-            val mood = cursor.getInt(ciMood)
-            val image = view?.findViewById<ImageView>(R.id.icon)
-            image?.setImageResource(
-                when (mood) {
-                    MOOD_FINE -> {
-                        R.drawable.ic_smiley_fine
-                    }
-                    MOOD_OK -> {
-                        R.drawable.ic_smiley_ok
-                    }
-                    else -> {
-                        R.drawable.ic_smiley_bad
-                    }
-                }
-            )
-        }
-        val ciTimeMillis = cursor?.getColumnIndex(MOOD_TIME)
-        ciTimeMillis?.let {
-            val timeMillis = cursor.getLong(ciTimeMillis)
-            date.time = timeMillis
-            val textview1 = view?.findViewById<TextView>(R.id.text1)
-            textview1?.text = dateFormat.format(date)
-            val textview2 = view?.findViewById<TextView>(R.id.text2)
-            textview2?.text = timeFormat.format(date)
-        }
+        cursor ?: return
+        val ciMood = cursor.getColumnIndex(MOOD_MOOD)
+        val mood = cursor.getInt(ciMood)
+        val image = view?.findViewById<ImageView>(R.id.icon)
+        image?.setImageResource(
+            when (mood) {
+                MOOD_FINE -> R.drawable.ic_smiley_fine
+                MOOD_OK -> R.drawable.ic_smiley_ok
+                else -> R.drawable.ic_smiley_bad
+            }
+        )
+        val ciTimeMillis = cursor.getColumnIndex(MOOD_TIME)
+        val timeMillis = cursor.getLong(ciTimeMillis)
+        date.time = timeMillis
+        val textview1 = view?.findViewById<TextView>(R.id.text1)
+        textview1?.text = dateFormat.format(date)
+        val textview2 = view?.findViewById<TextView>(R.id.text2)
+        textview2?.text = timeFormat.format(date)
     }
 }
