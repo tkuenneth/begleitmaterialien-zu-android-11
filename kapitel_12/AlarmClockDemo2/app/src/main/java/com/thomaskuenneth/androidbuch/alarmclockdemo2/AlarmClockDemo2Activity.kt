1708420b4e17111b312e4f8640a409453d9743b5
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-10 12:55:54 +0200

    Project AlarmClockDemo2 added

---
+++ kapitel_12/AlarmClockDemo2/app/src/main/java/com/thomaskuenneth/androidbuch/alarmclockdemo2/AlarmClockDemo2Activity.kt
@@ -0,0 +1,29 @@
+package com.thomaskuenneth.androidbuch.alarmclockdemo2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class AlarmClockDemo2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        textview.text = getString(R.string.no_intent)
+        intent?.run {
+            textview.text = String.format("%s\n", action)
+            extras?.let { bundle ->
+                bundle.keySet()?.let { keys ->
+                    keys.forEach {
+                        textview.append("$it\n")
+                        textview.append("$bundle[it]\n\n")
+                    }
+                }
+            }
+        }
+    }
+}
