6df5dcd491b3a1b6980736c96febe58ed9fc87c7
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-09-17 16:29:54 +0200

    Update HalloAndroidComposeActivity.kt

--- anhang_d/HalloAndroidCompose/app/src/main/java/com/thomaskuenneth/androidbuch/halloandroidcompose/HalloAndroidComposeActivity.kt
+++ anhang_d/HalloAndroidCompose/app/src/main/java/com/thomaskuenneth/androidbuch/halloandroidcompose/HalloAndroidComposeActivity.kt
@@ -6,15 +6,12 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.ColumnScope.gravity
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.*
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import androidx.ui.tooling.preview.Preview
 
 private val HEIGHT = 96.dp
